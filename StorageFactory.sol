898895a6e9b96bdaeb5ea13444c73e65ea7e3a06
Anthony Diké <anthonydike@nyu.edu>
2021-09-21 18:05:05 -0400

    feat: created StorageFactory smart contract

---
+++ StorageFactory.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+/* What I learned:
+* 1. How to deploy a contract from within a contract to testnet/mainnet
+* 2. How inheritance works with smart contracts
+* 3. How to interact with contract outside of my contract
+*/
+
+pragma solidity ^0.6.0;
+
+import "./SimpleStorage.sol";
+
+contract StorageFactory is SimpleStorage {
+    
+    SimpleStorage[] public simpleStorageArray;
+    
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+    
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        // Address 
+        // ABI
+        
+        SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
+    }
+    
+    function sfRetrieve(uint256 _simpleStorageIndex) public view returns(uint256){
+        return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
+    }
+    
+}
