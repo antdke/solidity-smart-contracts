0ada32cca070cc4703c016516f772cfe75206e6a
Anthony Diké <anthonydike@nyu.edu>
2021-09-30 18:50:39 -0400

    feat: created getConversionRate()

--- FundMe.sol
+++ FundMe.sol
@@ -11,14 +11,22 @@
 pragma solidity >=0.6.6 <0.9.0;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
 
 contract FundMe {
+    // used to prevent overflow issue when doing math in solidity. however, version .8 and onwards makes this useless
+    using SafeMathChainlink for uint256;
     
     // to keep track of who funded us (storing addresses and total amount they've sent)
     mapping (address => uint256) public addressToAmountFunded;
     
     // a function that accepts payments
     function fund() public payable {
+        // make $50 the min amount that someone can send this contract
+        uint256 minimumUSD = 50 * 10 ** 18;
+        // if person doesn't send the min required $ then tx is cancelled and they get their money back
+        require(getConversionRate(msg.value) >= minimumUSD, "You need to spend more ETH!");
+        // if tx meets the min required amount then record who send it
         addressToAmountFunded[msg.sender] += msg.value;
     }
     
@@ -40,7 +48,14 @@ contract FundMe {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
         // latestRoundData returns 5 values, so we deconstruct it into 5 variables
         (, int256 answer,,,) = priceFeed.latestRoundData();
-        return uint256(answer);
+        return uint256(answer * 10000000000); // converts to wei amount
+    }
+    
+    // convert wei we've been given into USD
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountToUSD = (ethPrice * ethAmount) / 1000000000000000000;
+        return ethAmountToUSD;
     }
     
 }
